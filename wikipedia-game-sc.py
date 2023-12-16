f675c9dd6a08a8a3d219363aef08eb5da082308d
AidanVazq Vazquez <avr.25dogs@gmail.com>
2023-12-16 07:19:56 +0000

    wikigame doneso

--- wikipedia-game-sc.py
+++ wikipedia-game-sc.py
@@ -5,8 +5,9 @@ user_agent = "MsOrret'sWikipediaGame/1.0 (orret.deborah@pusd.us)"
 
 wiki_wiki = wikipediaapi.Wikipedia(user_agent, "en")
 
-
 # HELPER FUNCTION: fetch_links(page) passes in a wikipedia page and returns a list of all the pages linked from that page
+
+
 def fetch_links(page):
     links_list = []
     links = page.links
@@ -15,13 +16,16 @@ def fetch_links(page):
 
     return links_list
 
+
 #IN CLASS: Finish the definition of the wikipedia_game_solver using a Breadth-First-Search Traversal
+
+
 def wikipedia_game_solver(start_page, target_page):
-    print('Working on it...')
-    start_time = time.time()
+    print('Working on it.........')
+    start_time = time.time( )
   
     # FINISH THE CODE HERE
-    end_time = time.time()
+    end_time = time.time( )
     print("This algorithm took", end_time-start_time, "seconds to run!")
   
     return path
