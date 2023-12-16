e5142547f0bfe7b046f57cb32a4ee238090205e0
AidanVazq Vazquez <avr.25dogs@gmail.com>
2023-12-16 07:17:42 +0000

    river probelem donso

--- river-problem.py
+++ river-problem.py
@@ -2,6 +2,10 @@ import copy
 
 
 # Define a function that takes in a state as a dictionary and returns True if the state meets the conditions and False if it does not
+
+
+
+
 def isValid(state):
     if state["wolf"] == state["goat"] and state["wolf"] != state["person"]:
         return False
@@ -9,12 +13,11 @@ def isValid(state):
         return False
     else:
         return True
-
-
-
-
 # Define a function that takes in a state as a dictionary and returns a list of all valid states that can be reached from 1 move of the input state
 # This function will need to call the function isValid(state)
+
+
+
 def get_next_states(state):
 
     next_states = []
@@ -42,10 +45,14 @@ def get_next_states(state):
 
 # Define a recursive function that takes in a current_state and win_state and returns the path to those states using the Depth First Search algorithm
 # This function will need to call the function get_next_states(state), as well as itself
+
+
+
+
 def dfs(current_state, win_state):
 
     if current_state == win_state:
-        return True  # Goal state reached
+        return True
 
     visited_states.append(current_state)
 
@@ -55,11 +62,12 @@ def dfs(current_state, win_state):
         if next_state not in visited_states:
             path.append(next_state)
             if dfs(next_state, win_state):
-                return True  # Goal state found
-            path.pop()
+                return True
+            path.pop( )
 
 
 # Test your code! Does it solve the river crossing riddle?
+
 initial_state = {
     "wolf": False,
     "goat": False,
